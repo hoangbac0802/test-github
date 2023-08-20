b5147f45ea1d73739cc68595a87b6dd6fafffb86
hoangbac0802 <bachoang0802@gmail.com>
2023-08-20 20:15:45 +0700

    bac

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace myprogram {
+    class Program {
+    static void Main (string[] args) {
+        Console.WriteLine (10);
+        Console.WriteLine(10f);
+        Console.WriteLine (true);
+    }
+}
+}
